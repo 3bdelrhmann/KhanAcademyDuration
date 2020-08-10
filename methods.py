c07f3a03ef5a460b1e52a7c8b65ff78d7bdc5e7a
Abdelrhman Mohamed <Abdelrhman_M@outlook.com>
2020-08-11 00:54:04 +0200

    compelete and improve methods

--- methods.py
+++ methods.py
@@ -8,16 +8,17 @@ class Main():
     UNIT         = 'UNIT'
     TOPIC        = 'TOPIC'
     
-    BRANCH_HEADER_ATTR  = 'data-test-id'
-    BRANCH_HEADER_VAL   = 'unit-header'
+    BRANCH_TITLE_ATTR  = 'data-test-id'
+    BRANCH_TITLE_VAL   = 'unit-header'
 
-    UNIT_HEADER_ATTR  = 'data-test-id'
-    UNIT_HEADER_VAL   = 'lesson-link'
+    UNIT_TITLE_ATTR  = 'data-test-id'
+    UNIT_TITLE_VAL   = 'lesson-link'
 
     TOPIC_HEADER_ATTR  = 'data-test-id'
     TOPIC_HEADER_VAL   = 'lesson-card-link'
 
-    LESSON_TITLE_CLASS = 'div[data-test-id="lesson-card"] ._14hvi6g8'
+    LESSON_TITLE_SELECTOR = 'div[data-test-id="lesson-card"] ._14hvi6g8'
+    LESSON_LINK_SELECTOR  = 'div[data-test-id="lesson-card"] ._stw1dyg > a:first-child'
 
     LESSON_LENGTH_ATTR = 'data-test-id'
     LESSON_LENGTH_VAL  = 'video-time-hidden'
@@ -34,7 +35,7 @@ class Main():
         """
             Determine web page is 
         """
-        web_page = ''
+        load_page = self.load_webpage(link)
         result   = self.CURRICULUM
         
         if self.BRANCHES_UNITS_CLASS not in web_page:
@@ -52,8 +53,8 @@ class Main():
             Units Links
         """
         page_content = self.load_webpage(self.link)
-        branches = page_content.find_all(attrs={self.BRANCH_HEADER_ATTR:self.BRANCH_HEADER_VAL},href=True)
-        units    = page_content.find_all(attrs={self.UNIT_HEADER_ATTR:self.UNIT_HEADER_VAL},href=True)
+        branches = page_content.find_all(attrs={self.BRANCH_TITLE_ATTR:self.BRANCH_TITLE_VAL},href=True)
+        units    = page_content.find_all(attrs={self.UNIT_TITLE_ATTR:self.UNIT_TITLE_VAL},href=True)
 
         self.branches_len   = len(branches)
         self.brances_titles = []
@@ -64,15 +65,37 @@ class Main():
         for branche in branches:
             for title in branche.contents:
                 self.brances_titles.append(title.get_text())
+    
+    def is_branch(self,branch_link):
+        """
+            units and topics len and titles
+        """
+        # NOTICE : I doesn't made a one method to get units titles in general
+        # and call these method in is_curriculm method and is_branch method
+        # because the selector in branch web page and in curriculm web page is Different
+        load_branch = self.load_webpage(branch_link)
+        # see deffrence between curriculum page VS branch page
+    #   units       = page_content.find_all(attrs={self.UNIT_TITLE_ATTR:self.UNIT_TITLE_VAL},href=True)
+        get_units   = load_branch.find_all(attrs={self.BRANCH_TITLE_ATTR:self.BRANCH_TITLE_VAL})
+        self.units_titles = []
+        self.total_units  = len(get_units)
         
-    def count_topics(self,unit_link):
-        load_unit = self.load_webpage(unit_link)
-        get_topics  = load_unit.select(self.LESSON_TITLE_CLASS)
-        get_lessons = load_unit.find_all(attrs={self.TOPIC_HEADER_ATTR:self.TOPIC_HEADER_VAL})
-
+        for unit in get_units:
+            for unit_title in unit.contents:
+                self.units_titles.append(unit_title.get_text())
+        
+    def is_unit(self,unit_link):
+        load_unit  = self.load_webpage(unit_link)
+        get_topics = load_unit.find_all(attrs={self.TOPIC_HEADER_ATTR:self.TOPIC_HEADER_VAL})
+        
+        get_lessons       = load_unit.select(self.LESSON_TITLE_SELECTOR)
+        get_lessons_link  = load_unit.select(self.LESSON_LINK_SELECTOR)
+        
         self.topics_titles  = [ topic.get_text() for topic in get_topics ]
         self.lessons_titles = [ lesson.get_text() for lesson in get_lessons ]
+        self.lessons_links  = [ lesson['href'] for lesson in get_lessons_link ] 
         self.total_lessons  = len(get_lessons)
+
         
     def lesson_length(self,lesson_link):
         load_lesson_page = self.load_webpage(lesson_link)
@@ -82,9 +105,5 @@ class Main():
         position    = total_duration.find(duration) + len(duration) # + len(duration) to clean the result from "duration:"
         
         return total_duration[position:] 
-    def count_units(self):
-        pass
 
 obj    = Main('https://www.khanacademy.org/math/high-school-math/')
-lesson_length = obj.lesson_length('https://www.khanacademy.org/math/high-school-math/math1/x89d82521517266d4:algebra-foundation/x89d82521517266d4:overview-history-alg/v/origins-of-algebra')
-print(lesson_length)
